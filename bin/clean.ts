7d9a671562b9d2fa96d070b8b4310348748b32df
ansanloms <ansanloms@users.noreply.github.com>
2024-03-01 17:44:31 +0900

    fix: remove clean.

--- bin/clean.ts
+++
@@ -1,6 +0,0 @@
-import * as path from "std/path/mod.ts";
-import * as fs from "std/fs/mod.ts";
-
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
-
-await fs.emptyDir(path.join(__dirname, `../dist`));
